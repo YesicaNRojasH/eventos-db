b31a06c006cd7f9df5c911fc151487638b4f4080
YESICA ROJAS <89708088+YesicaNRojasH@users.noreply.github.com>
2023-11-12 14:31:59 -0500

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -16,5 +16,5 @@ INSERT INTO Events (Title, Detail, [Date], CreatedAt, UpdatedAt) VALUES
 (N'Docker for Beginners', N'Introduction to Docker using Node.js', '2017-11-21', GETDATE(), GETDATE()),
 (N'Advanced Orchestration', N'Deep dive into Docker Swarm', '2017-12-25', GETDATE(), GETDATE()),
 (N'Docker on Windows', N'From 101 to production', '2018-01-01', GETDATE(), GETDATE());
-
+GO
 SELECT * FROM BulletinBoard.dbo.Events;
